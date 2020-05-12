5f4cf609a018dcf645ab71efeac7dfb3039598d3
epstlight <heewkk@gmail.com>
2020-05-12 17:33:23 +0900

    test1

--- src/main/java/com/ep/jo/HelloContorller.java
+++ src/main/java/com/ep/jo/HelloContorller.java
@@ -1,10 +1,12 @@
 package com.ep.jo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloContorller {
 	
+	@GetMapping("/")
 	public String hello() {
 		return "hello";
 	}
